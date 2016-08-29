c9646a4bdd01d1b85236055fcf1cbf6da23eb853
Trish Griego <pmgriego@gmail.com>
2016-08-29 15:58:55 -0700

    Worked on building roman numerals test case

--- lib/Roman_Number.rb
+++ lib/Roman_Number.rb
@@ -5,9 +5,38 @@ class Roman_Number
   def initialize(number)
     @number = number
   end
+# 5 = V
+# 6 = VI
+# 7 = VII
+# 8 = VIII
+
+
+
 
   def get_roman()
-    "I"
+    num = @number
+    answer = ""
+    while num >= 10
+      answer += "X"
+      num    -= 10
+    end
+    if num == 9
+      answer += "IX"
+      num -= 9
+    end
+    if num >= 5
+      answer += "V"
+      num -= 5
+    end
+    if num == 4
+      answer += "IV"
+      num -= 4
+    end
+    answer += "I" * num # * @number
+    return answer
   end
 
 end
+
+
+# if you can reduce this method down with an array and loop.
--- specs/Roman_spec.rb
+++ specs/Roman_spec.rb
@@ -14,3 +14,19 @@ describe Roman_Number do
     expect( Roman_Number.new(2).get_roman ).must_equal("II")
     expect( Roman_Number.new(3).get_roman ).must_equal("III")
   end
+  it '4 Converts to a Roman Numeral' do
+    expect( Roman_Number.new(4).get_roman ).must_equal("IV")
+  end
+  it '7 Converts to a Roman Numeral' do
+    expect( Roman_Number.new(7).get_roman ).must_equal("VII")
+  end
+  it '9 Converts to a Roman Numeral' do
+    expect( Roman_Number.new(9).get_roman ).must_equal("IX")
+  end
+  it '10 Converts to a Roman Numeral' do
+    expect( Roman_Number.new(10).get_roman ).must_equal("X")
+  end
+  it '14 Converts to a Roman Numeral' do
+    expect( Roman_Number.new(14).get_roman ).must_equal("XIV")
+  end
+end
